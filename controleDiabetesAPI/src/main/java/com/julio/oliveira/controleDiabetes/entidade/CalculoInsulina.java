354315901bd1420f0ddb5b62de7755821743dbe9
jcfoj <jcfoj1993@gmail.com>
2021-11-23 11:47:34 -0300

    Back-end - Mapeamento de relacionamento entre tabelas

--- controleDiabetesAPI/src/main/java/com/julio/oliveira/controleDiabetes/entidade/CalculoInsulina.java
+++ controleDiabetesAPI/src/main/java/com/julio/oliveira/controleDiabetes/entidade/CalculoInsulina.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -20,15 +22,18 @@ public class CalculoInsulina extends ClasseBase {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer codCalculoInsulina;
 	
-	@Column(name="COD_TIPO_CALCULO")
+	@ManyToOne()
+	@JoinColumn(name="COD_TIPO_CALCULO_INSULINA")
 	@NotNull
 	private TipoCalculoInsulina tipoCalculoInsulina;
 	
-	@Column(name="COD_GLICEMIA")
+	@ManyToOne()
+	@JoinColumn(name="COD_GLICEMIA")
 	@NotNull
 	private Glicemia glicemia;
 	
-	@Column(name="COD_ALIMENTO")
+	@ManyToOne()
+	@JoinColumn(name="COD_ALIMENTO")
 	@NotNull
 	private Alimento alimento;
 	
